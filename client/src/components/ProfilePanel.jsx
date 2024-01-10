6d1aafb1be96dea0f5b168576c5ee31aeef0d5f1
Sebastian Jablonski <s.jablonski.684@studms.ug.edu.pl>
2024-01-10 22:05:05 +0100

    Added option to delete user

--- client/src/components/ProfilePanel.jsx
+++ client/src/components/ProfilePanel.jsx
@@ -1,26 +1,53 @@
 import React,{useContext, useEffect, useState} from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Avatar from '@mui/material/Avatar';
 import { ThemeContext } from 'context/ThemeContext';
 import EditIcon from '@mui/icons-material/Edit';
 import UpdateForm from './UpdateForm';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { setLogout } from 'reducer';
 
 function ProfilePanel() {
     const [edit, setEdit] = useState(false);
     const [render, setRender] = useState(false);
     const user = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const dispatch = useDispatch();
     const {mode, setMode} = useContext(ThemeContext);
 
+    async function deleteAccount(id) {
+            const deleteUserRes = await fetch(`http://localhost:3001/user/${user._id}/delete`, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+            }})
+            const deleteUserJson = await deleteUserRes.json();
+            if (deleteUserRes.status === 200) {
+                dispatch(setLogout());
+            }
+            
+           
+        }
     
     return (
         <div className="flex flex-col border border-black bg-white text-black rounded-xl mr-2 p-4">
             <div className="flex justify-between">
                 <Avatar alt={user.firstName} src="../public/assets/twitter.png" />
-                <button className='mr-2' onClick={
-                    () => setEdit(!edit)
-                }>
-                    <EditIcon />
-                </button>
+                    <div>
+                        <button className='mr-2' onClick={
+                            () => setEdit(!edit)
+                        }>
+                            <EditIcon />
+                        </button>
+                        <button onClick={
+                            () => deleteAccount(user._id)
+
+                        }>
+                            <DeleteIcon />
+                        </button>    
+                    </div>
 
             </div>
             <p>First Name: {user.firstName}</p>
--- client/src/scenes/homePage/HomePage.jsx
+++ client/src/scenes/homePage/HomePage.jsx
@@ -4,10 +4,15 @@ import { useSelector } from 'react-redux'
 import { ThemeContext } from 'context/ThemeContext';
 import NavBar from 'scenes/navbar/NavBar';
 import FeedPosts from 'components/FeedPosts';
+import { Navigate } from 'react-router-dom';
 
 function HomePage() {
   const user = useSelector((state) => state.user);
   const { mode, setMode } = useContext(ThemeContext);
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div className="text-slate-800 size-xl">
